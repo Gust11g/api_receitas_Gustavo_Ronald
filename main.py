31bfc1769da7d8ff0c896d958fe616d6cdcc5118
Francisco Gustavo da Silva Sousa <francisco.gustavo71@aluno.ifce.edu.br>
2025-08-26 11:27:15 +0000

    3 receitas

---
+++ main.py
@@ -0,0 +1,53 @@
+receitas = [
+{
+"nome": "Brigadeiro Tradicional",
+"descricao": "O doce mais amado do Brasil, perfeito para festas e para qualquer momento!",
+"ingredientes": [
+"1 lata de leite condensado",
+"1 colher de sopa de manteiga sem sal",
+"4 colheres de sopa de chocolate em pó",
+"Chocolate granulado para decorar"
+],
+"utensílios": [
+"panela",
+"prato",
+"forminhas de papel"
+],
+"modo de preparo": "Em uma panela, coloque o leite condensado, a manteiga e o chocolate em pó. Cozinhe em fogo médio, mexendo sempre, até a mistura desgrudar do fundo da panela (cerca de 10 minutos). Despeje em um prato untado com manteiga e deixe esfriar. Com as mãos untadas, enrole pequenas bolinhas e passe no chocolate granulado. Sirva em forminhas de papel."
+},
+{
+  "nome": "Bolo de Fubá da Vovó",
+  "descricao": "Um bolo simples, fofinho e perfeito para o café da tarde.",
+  "ingredientes": [
+    "2 xícaras de açúcar",
+    "3 xícaras de fubá",
+    "2 xícaras de farinha de trigo",
+    "1 xícara de óleo",
+    "4 ovos",
+    "2 xícaras de leite",
+    "1 colher de sopa de fermento em pó"
+  ],
+  "utensílios": [
+    "liquidificador",
+    "tigela",
+    "forma"
+  ],
+  "modo de preparo": "No liquidificador, bata os ovos, o açúcar, o óleo e o leite. Em uma tigela, misture o fubá, a farinha de trigo e o fermento. Despeje a mistura líquida na tigela com os ingredientes secos e mexa bem até a massa ficar homogênea. Coloque a massa em uma forma untada e enfarinhada. Leve ao forno pré-aquecido a 180°C por aproximadamente 40 minutos, ou até dourar."
+},
+{
+  "nome": "Mousse de Maracujá Rápido",
+  "descricao": "Uma sobremesa refrescante, fácil e que leva apenas 3 ingredientes.",
+  "ingredientes": [
+    "1 lata de leite condensado",
+    "1 lata de creme de leite",
+    "1 lata (use a mesma medida) de suco de maracujá concentrado"
+  ],
+  "utensílios": [
+    "liquidificador",
+    "taças individuais",
+    "refratário"
+  ],
+  "modo de preparo": "Coloque todos os ingredientes no liquidificador. Bata por aproximadamente 3 minutos, até obter uma mistura cremosa e consistente. Despeje em taças individuais ou em um refratário. Leve à geladeira por pelo menos 2 horas antes de servir."
+},
+
+]
