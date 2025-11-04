b744a236bebc6974dc8dfe8a8fabbce1be9138a9
Francisco Gustavo da Silva Sousa <francisco.gustavo71@aluno.ifce.edu.br>
2025-11-04 11:06:09 +0000

    Alterações

--- schema.py
+++ schema.py
@@ -1,16 +1,31 @@
 from pydantic import BaseModel
 from typing import List
-from http import HTTPStatus
-
-app = FastAPI(title="Livro de Receitas")
-
-class CreateReceita(BaseModel):
-    nome: str
-    ingredientes: List[str]
-    modo_de_preparo: str
 
 class Receita(BaseModel):
     id: int
     nome: str
     ingredientes: List[str]
     modo_de_preparo: str
+
+class Create_Receita(BaseModel):
+    nome: str
+    ingredientes: List[str]  # lista normal
+    modo_de_preparo: str
+
+class Usuario(BaseModel):
+    id: int
+    nome_usuario: str
+    email: str
+    senha: str
+
+class BaseUsuario(BaseModel):
+    nome_usuario: str
+    email: str
+    senha: str
+
+class UsuarioPublic(BaseModel):
+    id:int
+    nome_usuario:str
+    email:str
+    
+
